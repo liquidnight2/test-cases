276fa8d55387ab7991306f4319368502f7dc1f7b
marcus <marcus@kav>
2020-10-16 01:19:59 +0200

    added button_press_event

--- gtk-rs-rightmouse/src/main.rs
+++ gtk-rs-rightmouse/src/main.rs
@@ -1,6 +1,6 @@
 //! # TreeView Sample
+//! modified by connect_button_press_event
 //!
-//! This sample demonstrates how to create a TreeView with a ListStore.
 
 extern crate gio;
 extern crate gtk;
@@ -15,10 +15,7 @@ use gtk::{
 use std::env::args;
 
 fn create_and_fill_model() -> ListStore {
-    // Creation of a model with two rows.
     let model = ListStore::new(&[u32::static_type(), String::static_type()]);
-
-    // Filling up the tree view.
     let entries = &["Michel", "Sara", "Liam", "Zelda", "Neo", "Octopus master"];
     for (i, entry) in entries.iter().enumerate() {
         model.insert_with_values(None, &[0, 1], &[&(i as u32 + 1), &entry]);
@@ -29,19 +26,14 @@ fn create_and_fill_model() -> ListStore {
 fn append_column(tree: &TreeView, id: i32) {
     let column = TreeViewColumn::new();
     let cell = CellRendererText::new();
-
     column.pack_start(&cell, true);
-    // Association of the view's column with the model's `id` column.
     column.add_attribute(&cell, "text", id);
     tree.append_column(&column);
 }
 
 fn create_and_setup_view() -> TreeView {
-    // Creating the tree view.
     let tree = TreeView::new();
-
     tree.set_headers_visible(false);
-    // Creating the two columns inside the view.
     append_column(&tree, 0);
     append_column(&tree, 1);
     tree
@@ -49,53 +41,42 @@ fn create_and_setup_view() -> TreeView {
 
 fn build_ui(application: &gtk::Application) {
     let window = ApplicationWindow::new(application);
-
     window.set_title("Simple TreeView example");
     window.set_position(WindowPosition::Center);
-
-    // Creating a vertical layout to place both tree view and label in the window.
     let vertical_layout = gtk::Box::new(Orientation::Vertical, 0);
-
-    // Creation of the label.
     let label = Label::new(None);
-
     let tree = create_and_setup_view();
-
     let model = create_and_fill_model();
-    // Setting the model into the view.
     tree.set_model(Some(&model));
-    // Adding the view to the layout.
     vertical_layout.add(&tree);
-    // Same goes for the label.
     vertical_layout.add(&label);
 
-    // The closure responds to selection changes by connection to "::cursor-changed" signal,
-    // that gets emitted when the cursor moves (focus changes).
     tree.connect_cursor_changed(move |tree_view| {
         let selection = tree_view.get_selection();
         if let Some((model, iter)) = selection.get_selected() {
-            // Now getting back the values from the row corresponding to the
-            // iterator `iter`.
-            //
-            // The `get_value` method do the conversion between the gtk type and Rust.
-            label.set_text(&format!(
-                "Hello '{}' from row {}",
-                model
-                    .get_value(&iter, 1)
-                    .get::<String>()
-                    .expect("Treeview selection, column 1")
-                    .expect("Treeview selection, column 1: mandatory value not found"),
-                model
-                    .get_value(&iter, 0)
-                    .get_some::<u32>()
-                    .expect("Treeview selection, column 0"),
-            ));
+            let val = model.get_value(&iter, 1).get::<String>().unwrap();
+            let idx = model.get_value(&iter, 0).get_some::<u32>().unwrap();
+            label.set_text(&format!("Hello '{:?}' from row {:?}", val, idx));
+            println!("cursor_changed '{:?}' from row {:?}", val, idx);
         }
     });
 
-    // Adding the layout to the window.
-    window.add(&vertical_layout);
+    tree.connect_button_press_event(move |p_tv, eventbutton| {
+        let treeview: gtk::TreeView = p_tv.clone().dynamic_cast::<gtk::TreeView>().unwrap();
+        let button_num = eventbutton.get_button() as i32;
+        let selection = treeview.get_selection();
+        if let Some((model, iter)) = selection.get_selected() {
+            let val = model.get_value(&iter, 1).get::<String>().unwrap();
+            let idx = model.get_value(&iter, 0).get_some::<u32>().unwrap();
+            println!(
+                "button_press_event  button{} '{:?}' from row {:?}",
+                button_num, val, idx
+            );
+        }
+        gtk::Inhibit(false)
+    });
 
+    window.add(&vertical_layout);
     window.show_all();
 }
 
@@ -105,10 +86,8 @@ fn main() {
         Default::default(),
     )
     .expect("Initialization failed...");
-
     application.connect_activate(|app| {
         build_ui(app);
     });
-
     application.run(&args().collect::<Vec<_>>());
 }
