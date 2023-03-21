7ee7378378f256f319809f5dc456b0756a751bb5
liquidnight <liquidnight2@gmx.net>
2023-03-21 04:07:31 +0100

    step6, works

--- dependencyresolution/analyser/src/lib.rs
+++ dependencyresolution/analyser/src/lib.rs
@@ -1,29 +1,18 @@
-#[cfg(feature = "dep_new")]
+#[cfg(feature = "a_new")]
 extern crate dep_new as dep;
-#[cfg(feature = "dep_old")]
+
+#[cfg(feature = "a_old")]
 extern crate dep_old as dep;
 
-
-use dep::image as image;
-
-
-use image::io::DynamicImage;
-use image::io::Reader;
+use dep::image::DynamicImage;
+use dep::image::io::Reader;
 use std::error::Error;
 
 pub fn analyse_image(name: &str) -> Result<(), Box<dyn Error>> {
-    println!("opening: {}", name);
     let r_open = Reader::open(name)?;
     let r_decoded: DynamicImage = r_open.decode()?;
-
     if r_decoded.height() > 0 {
         return Ok(());
     }
     Result::Err("no height")?
-}
-
-#[cfg(test)]
-mod tests {
-    // use super::*;
-    // #[test]    fn it_works() {        let result = add(2, 2);        assert_eq!(result, 4);    }
 }
