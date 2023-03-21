e05e31d3f7df96bd99afb69ca8d9b4adc7a093fd
liquidnight <liquidnight2@gmx.net>
2023-03-21 04:17:54 +0100

    works for now

--- dependencyresolution/analyser/src/lib.rs
+++ dependencyresolution/analyser/src/lib.rs
@@ -4,13 +4,14 @@ extern crate dep_new as dep;
 #[cfg(feature = "a_old")]
 extern crate dep_old as dep;
 
-use dep::image::DynamicImage;
 use dep::image::io::Reader;
+use dep::image::{DynamicImage, GenericImageView};
 use std::error::Error;
 
 pub fn analyse_image(name: &str) -> Result<(), Box<dyn Error>> {
     let r_open = Reader::open(name)?;
     let r_decoded: DynamicImage = r_open.decode()?;
+
     if r_decoded.height() > 0 {
         return Ok(());
     }
